03c92baf4b5294eaf6368f474c8d9927087b3a05
Wellington Castro <wellington.castro@rw3tecnologia.com>
2024-04-21 13:56:37 -0300

    Melhorias de UI/UX

--- src/app/components/my-decks/my-decks.component.ts
+++ src/app/components/my-decks/my-decks.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core
 import { DeckService } from '../../services/deck.service';
 import { Service } from '../../services/service';
 import { ActionsComponent } from '../actions/actions.component';
+import { Card } from '../../models/deck';
 
 @Component({
   selector: 'app-my-decks',
@@ -16,9 +17,35 @@ export class MyDecksComponent {
   @Output() modeDeckCards: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() deckCards: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() cards: EventEmitter<any[]> = new EventEmitter<any[]>();
+  detailRotate: boolean = false;
+  trainerCardCount: number = 0;
+  typeCounts: { [key: string]: number } = {};
 
   constructor(private deckService: DeckService, private service: Service) {
     this.actionsComponent!;
+  }
+
+  showDetails(deck: any) {
+    this.detailRotate = true;
+    let trainerCardCount = 0;
+    for (const card of deck.cards) {
+      if (card.supertype === 'Trainer') {
+        trainerCardCount++;
+        this.trainerCardCount = trainerCardCount;
+      }
+    }
+    const typeCounts: { [key: string]: number } = {};
+    for (const card of deck.cards) {
+      for (const type of card.types) {
+        if (typeCounts[type]) {
+          typeCounts[type]++;
+        } else {
+          typeCounts[type] = 1;
+        }
+        this.typeCounts = typeCounts;
+      }
+    }
+    this.typeCounts = typeCounts;
   }
 
   toggleModeDeckCards(deck: any) {
@@ -29,10 +56,14 @@ export class MyDecksComponent {
     this.cards.emit(deck.cards);
   }
 
-  removeDeck(deck: any) {
+  removeDeck(deck: Card) {
     console.log('removing deck'); 
     this.deckService.removeDeck(deck.id);
     this.removeDeckClicked.emit();
+  }
+
+  close() {
+    this.detailRotate = false;
   }
 
 }
